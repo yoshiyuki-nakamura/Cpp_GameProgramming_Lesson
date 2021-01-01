9bed8de32427f2e571e202c3df3b4f82a23d4b6d
ななしー <email@example.com>
2021-01-01 19:42:59 +0900

    OpenSiv3Dの新規プロジェクトを追加

---
+++ Cpp_GameProgramming_Lesson/Cpp_GameProgramming_Lesson/Main.cpp
@@ -0,0 +1,15 @@
+﻿// Siv3Dを使うために必要です。
+#include <Siv3D.hpp>
+
+/// <summary>
+/// Main関数です。
+/// ここからプログラムが始まります。
+/// </summary>
+void Main()
+{
+    // ゲームループです。
+    // このwhileの中にゲームプログラムを書きます。
+    while (System::Update())
+    {
+    }
+}
